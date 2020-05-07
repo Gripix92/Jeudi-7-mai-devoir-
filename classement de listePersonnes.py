ab97a39a7dd8ab0a407ee5f75ef483e85e999eab
Gripix92 <60234638+Gripix92@users.noreply.github.com>
2020-05-07 16:31:48 +0200

    Add files via upload

---
+++ classement de listePersonnes.py
@@ -0,0 +1,22 @@
+ListesEleves = [["Jeras","Yannis","Adem"],["Aminata","Aya","Rémi"]]
+ListeTrié = []
+
+def Tri():
+    for sous_listes in ListesEleves:
+        for items in sous_listes:
+            ListeTrié.append(items)
+    print(ListeTrié)
+
+def Dico():
+    Tri()
+    dico = {}
+    for itemsListeTrié in ListeTrié:
+        points = 0
+        for sous_listes in ListesEleves:
+            for itemsSousListes in sous_listes:
+                if itemsSousListes == itemsListeTrié:
+                    points += 100 - ( sous_listes.index(itemsSousListes) * 10 )
+        dico[itemsListeTrié] = str(int(points)) + " points"
+    print(dico)
+
+Dico()
